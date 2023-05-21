cdbb3c6abf708e6c9effbb6c95f0734ee69d15f1
yamadicu <dicutia@gmail.com>
2023-05-21 16:19:26 +0200

    fine compito

--- index.php
+++ index.php
@@ -53,9 +53,64 @@
 </head>
 <body>
 
-<?php
-var_dump($hotels);
-?>
+<div class="container mt-5">
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <?php 
+            foreach ($hotels as $element){
+                echo '<th scope="col">' .$element['name'] .'</th>';
+            }
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- nome hotel -->
+        <tr>
+            <th scope="row">descrizione</th>
+            <?php 
+            foreach ($hotels as $element){
+                echo '<td>' .$element['description'].'</td>';
+            }
+            ?>
+        </tr>
+        <!-- recensione -->
+        <tr>
+            <th scope="row">recensione</th>
+            <?php 
+            foreach ($hotels as $element){
+                echo '<td class="text-center">' .$element['vote'].'</td>';
+            }
+            ?>
+        </tr>
+        <!-- disponibilità parcheggio -->
+        <tr>
+            <th scope="row">parcheggio</th>
+            <?php 
+            foreach ($hotels as $element){
+                if($element['parking'] == true){
+                    echo '<td class="text-center">'.'il parcheggio è incluso nel prezzo'.'</td>';
+                }else{
+                    echo '<td class="text-center text-danger">'.'non è disponibile il parcheggio'.'</td>';
+                }
+            }
+            ?>
+        </tr>
+        <!-- distanza dal centro -->
+        <tr>
+            <th scope="row">chilometri dal centro</th>
+            <?php 
+            foreach ($hotels as $element){
+                echo '<td class="text-center">' .$element['distance_to_center'].' km'.'</td>';
+            }
+            ?>
+        </tr>
+    </tbody>
+</table>
+
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
